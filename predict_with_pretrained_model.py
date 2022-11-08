aa400b0d60ba1963313bc5565ab934903c1b645c
LainWiredIn <manav.chaudhary@research.iiit.ac.in>
2022-11-09 02:02:46 +0530

    pylint score 9.32

--- predict_with_pretrained_model.py
+++ predict_with_pretrained_model.py
@@ -7,16 +7,17 @@ import os
 # from random import shuffle
 import torch
 # import torch.nn as nn
+from torch_geometric.loader import DataLoader # pylint: disable=import-error
 from models.gat import GATNet
 from models.gat_gcn import GAT_GCN
 from models.gcn import GCNNet
 from models.ginconv import GINConvNet
-from torch_geometric.loader import DataLoader
 from utils import *
 
 datasets = ["davis", "kiba"]
 modelings = [GINConvNet, GATNet, GAT_GCN, GCNNet]
-cuda_name = "cuda:0"
+cuda_name = "cuda:0" # pylint: disable=invalid-name
+# cuda constant name uppercasing not possible
 
 TEST_BATCH_SIZE = 512
 
@@ -40,23 +41,21 @@ def predict(dataset, modeling):
     """
     modeling = modelings[modeling]
     processed_data_file_test = 'data/processed/test.pt'
-    if (not os.path.isfile(processed_data_file_test)):
+    if not os.path.isfile(processed_data_file_test):
         print('please run create_data.py to prepare data in pytorch format!')
         return 0
-    else:
-        test_data = TestbedDataset(root='data', dataset='test')
-        test_loader = DataLoader(test_data, batch_size=TEST_BATCH_SIZE, shuffle=False)
-        model_st = modeling.__name__
-        print("\npredicting for ", dataset, " using ", model_st)
-        # training the model
-        device = torch.device(cuda_name if torch.cuda.is_available() else "cpu") # pylint: disable=no-member
-        model = modeling().to(device)
-        model_file_name = "trained_models/model_" + model_st + "_" + dataset + ".model"
-        if os.path.isfile(model_file_name):
-            model.load_state_dict(torch.load(model_file_name, map_location=torch.device('cpu')))
-            predicted_affinity = predicting(model, device, test_loader)
-            print("The Predicted Drug Target Affinity is ", predicted_affinity)
-            return predicted_affinity
-        else:
-            print("model is not available!")
-            return 0
+    test_data = TestbedDataset(root='data', dataset='test')
+    test_loader = DataLoader(test_data, batch_size=TEST_BATCH_SIZE, shuffle=False)
+    model_st = modeling.__name__
+    print("\npredicting for ", dataset, " using ", model_st)
+    # training the model
+    device = torch.device(cuda_name if torch.cuda.is_available() else "cpu") # pylint: disable=no-member
+    model = modeling().to(device)
+    model_file_name = "trained_models/model_" + model_st + "_" + dataset + ".model"
+    if os.path.isfile(model_file_name):
+        model.load_state_dict(torch.load(model_file_name, map_location=torch.device('cpu'))) # pylint: disable=no-member
+        predicted_affinity = predicting(model, device, test_loader)
+        print("The Predicted Drug Target Affinity is ", predicted_affinity)
+        return predicted_affinity
+    print("model is not available!")
+    return 0
