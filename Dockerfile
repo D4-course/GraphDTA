175a3194e6336070c1531ea1413f3e72d063d070
Mugundan Kottursuresh <mugundan.kottur@gmail.com>
2022-11-08 22:35:54 +0530

    Dockerized

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apt-key del 7fa2af80
 RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
 
-RUN apt-get update && apt-get install -y wget curl unzip libxml2 cuda-minimal-build-11-3 libcusparse-dev-11-3 libcublas-dev-11-3 libcusolver-dev-11-3 git hmmer vim
+RUN apt-get update && apt-get install -y wget curl unzip libxml2 cuda-minimal-build-11-3 libcusparse-dev-11-3 libcublas-dev-11-3 libcusolver-dev-11-3 git hmmer vim gcc python3-dev
 
 RUN wget -P /tmp \
     "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" \
@@ -19,9 +19,43 @@ RUN wget -P /tmp \
     && rm /tmp/Miniconda3-latest-Linux-x86_64.sh
 ENV PATH /opt/conda/bin:$PATH
 
-COPY environment.yml /opt/graphdta/environment.yml
+# COPY environment.yml /opt/graphdta/environment.yml
 
 # installing into the base environment since the docker container wont do anything other than run GraphDTA
-RUN conda env update -n base --file /opt/graphdta/environment.yml && conda clean --all
+# RUN conda env update -n base --file /opt/graphdta/environment.yml && conda clean --all
 
-COPY graphdta.py /graphdta/main.py
+RUN conda install -y -c conda-forge rdkit
+
+RUN conda install pytorch torchvision cudatoolkit -c pytorch
+
+RUN conda install pyg -c pyg
+
+# RUN pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+
+# RUN pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+
+# RUN pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+
+# RUN pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+
+RUN pip install torch-geometric numpy pandas networkx
+
+RUN mkdir /GraphDTA
+
+RUN cd /GraphDTA
+
+COPY graphdta.py /GraphDTA/main.py
+
+COPY predict_with_pretrained_model.py /GraphDTA/predict_with_pretrained_model.py
+
+COPY utils.py /GraphDTA/utils.py
+
+COPY create_data.py /GraphDTA/create_data.py
+
+COPY trained_models /GraphDTA/trained_models
+
+COPY models /GraphDTA/models
+
+WORKDIR /GraphDTA
+
+CMD python3 /GraphDTA/main.py
--- create_data.py
+++ create_data.py
@@ -8,6 +8,11 @@ from rdkit.Chem import MolFromSmiles
 import networkx as nx
 from utils import *
 
+SEQ_VOC = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+SEQ_DICT = {v:(i+1) for i,v in enumerate(SEQ_VOC)}
+SEQ_DICT_LEN = len(SEQ_DICT)
+MAX_SEQ_LEN = 1000
+
 def atom_features(atom):
     return np.array(one_of_k_encoding_unk(atom.GetSymbol(),['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na','Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb','Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H','Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr','Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
                     one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6,7,8,9,10]) +
@@ -47,35 +52,31 @@ def smile_to_graph(smile):
     return c_size, features, edge_index
 
 def seq_cat(prot):
-    x = np.zeros(max_seq_len)
-    for i, ch in enumerate(prot[:max_seq_len]): 
-        x[i] = seq_dict[ch]
+    x = np.zeros(MAX_SEQ_LEN)
+    for i, ch in enumerate(prot[:MAX_SEQ_LEN]): 
+        x[i] = SEQ_DICT[ch]
     return x  
-    
-seq_voc = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-seq_dict = {v:(i+1) for i,v in enumerate(seq_voc)}
-seq_dict_len = len(seq_dict)
-max_seq_len = 1000
 
 compound_iso_smiles = []
 for dt_name in ['kiba','davis']:
     opts = ['train','test']
     for opt in opts:
-        df = pd.read_csv('data/' + dt_name + '_' + opt + '.csv')
+        df = pd.read_csv('./original/data/' + dt_name + '_' + opt + '.csv')
         compound_iso_smiles += list( df['compound_iso_smiles'] )
 compound_iso_smiles = set(compound_iso_smiles)
-smile_graph = {}
+SMILE_GRAPH = {}
 for smile in compound_iso_smiles:
     g = smile_to_graph(smile)
-    smile_graph[smile] = g
+    SMILE_GRAPH[smile] = g
 
-# convert to PyTorch data format
-processed_data_file_test = 'data/processed/test.pt'
-df = pd.read_csv('data/test.csv')
-test_drugs, test_prots,  test_Y = list(df['compound_iso_smiles']),list(df['target_sequence']),list(df['affinity'])
-XT = [seq_cat(t) for t in test_prots]
-test_drugs, test_prots,  test_Y = np.asarray(test_drugs), np.asarray(XT), np.asarray(test_Y)
-# make data PyTorch Geometric ready
-print('preparing ', dataset + '_test.pt in pytorch format!')
-test_data = TestbedDataset(root='data', dataset=dataset+'_test', xd=test_drugs, xt=test_prots, y=test_Y,smile_graph=smile_graph)
-print(processed_data_file_train, ' and ', processed_data_file_test, ' have been created')      
+def create_test(iso_smile, target):
+    # convert to PyTorch data format
+    processed_data_file_test = './data/processed/test.pt'
+    test_drugs = [iso_smile]
+    test_prots = [target]
+    test_Y = [1]
+    XT = [seq_cat(t) for t in test_prots]
+    test_drugs, test_prots,  test_Y = np.asarray(test_drugs), np.asarray(XT), np.asarray(test_Y)
+    # make data PyTorch Geometric ready
+    print('preparing test.pt in pytorch format!')
+    test_data = TestbedDataset(root='data', dataset='test', xd=test_drugs, xt=test_prots, y=test_Y,smile_graph=SMILE_GRAPH)
